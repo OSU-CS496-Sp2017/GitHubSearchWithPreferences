52632682fadcf971e458a53e44cfcc68e6c4dbdd
Rob Hess <robwhess@gmail.com>
2017-05-20 22:12:40 -0700

    Hook up settings activity and action to navigate to settings activity.

--- app/src/main/java/com/example/android/githubsearchwithpreferences/MainActivity.java
+++ app/src/main/java/com/example/android/githubsearchwithpreferences/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -63,6 +65,24 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void doGitHubSearch(String searchQuery) {
